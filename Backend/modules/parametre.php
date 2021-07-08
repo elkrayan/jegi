7961a72e597fccf1c2091e51640d769b5456fb54
Rayan El-Koukouchi <rayan@Rayans-MacBook-Pro.local>
2021-07-08 21:08:11 +0200

    pull fiunction

---
+++ Backend/modules/parametre.php
@@ -0,0 +1,48 @@
+<!-- Ajouter un form au dessus-->
+<div class="form-container">
+    <div class="form-item col">
+        <label for="ta">Tension Artiérielle</label>
+        <div class="row">
+            <input type="number" name="" id="" placeholder="systolique">
+            /
+            <input type="number" name="" id="" placeholder="diastolique">
+            mmgh
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-item col">
+            <label for="">FC</label>
+            <input type="number" name="" id="" placeholder="fréquence cardiaque">
+        </div>
+        <div class="form-item col">
+            <label for="">Saturation</label>
+            <input type="number" name="" id="" placeholder="saturation">
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-item col">
+            <label for="">Glycemie</label>
+            <input type="number">
+        </div>
+        <div class="form-item col">
+            <label for="">Température</label>
+            <input type="number">
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-item col">
+            <label for="">Poids</label>
+            <input type="number">
+        </div>
+        <div class="col">
+            <div class="form-item row">
+                <label for="">Critique</label>
+                <input type="checkbox" name="" id="">
+            </div>
+            <div class="form-item row">
+                <label for="">À risque</label>
+                <input type="checkbox" name="" id="">
+            </div>
+        </div>
+    </div>
+</div>
